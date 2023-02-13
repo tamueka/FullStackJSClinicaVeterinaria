49841558c78990482d15564d31042eafef35ca8c
= <=>
2023-02-13 20:28:02 +0100

    almacenar registro en BD + prevenir registro duplicado

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -1,6 +1,21 @@
-const registrar = (req, res) => {
-  const { nombre, email, password } = req.body;
-  res.json({ msg: "Registrando veterinario..." });
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) => {
+  const { email } = req.body;
+  // Prevenir veterinario duplicados
+  const existeUsuario = await Veterinario.findOne({ email });
+  if (existeUsuario) {
+    const error = new Error("Veterinario ya registrado");
+    return res.status(400).json({ msg: error.message });
+  }
+  try {
+    // Guardar nuevo veterinario
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+    res.json({ veterinarioGuardado });
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 const perfil = (req, res) => {
