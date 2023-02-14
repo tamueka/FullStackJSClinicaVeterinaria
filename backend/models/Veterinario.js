04779b4b7f3b6b6334aeca55e690998e89dcc9a4
= <=>
2023-02-14 20:12:02 +0100

    hasheamos los password de lso usuarios con la libreria bcrypt

--- backend/models/Veterinario.js
+++ backend/models/Veterinario.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 import generarID from "../helpers/generarId.js";
 
 const veterinarioSchema = mongoose.Schema({
@@ -29,6 +30,15 @@ const veterinarioSchema = mongoose.Schema({
     type: Boolean,
     default: false,
   },
+});
+
+veterinarioSchema.pre("save", async function (next) {
+  // si el password ya esta hasheado pasamos a la siguinete funcion del middleware con next()
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const Veterinario = mongoose.model("Veterinario", veterinarioSchema);
