b3ab564e9aa2dd39ef9a4bcb35ac70ec70313af6
= <=>
2023-02-17 19:20:32 +0100

    pasos para autenticar usuario + usuario existes + usuario confirmado + comprobarmos password de usuario (creamos funcion asincrona para comprobar password con bcrypt) + creacion ruta nueva

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -43,4 +43,30 @@ const confirmar = async (req, res) => {
   }
 };
 
-export { registrar, perfil, confirmar };
+const autenticar = async (req, res) => {
+  const { email, password } = req.body;
+
+  const usuario = await Veterinario.findOne({ email });
+
+  // Comprobar si el usuario existe
+  if (!usuario) {
+    const error = new Error("Email no válido");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  // Comprobar si el usuario esta confirmado
+  if (!usuario.confirmado) {
+    const error = new Error("Usuario no confirmado");
+    return res.status(401).json({ msg: error.message });
+  }
+
+  // Comprobar si el password es igual
+  if (await usuario.comprobarPassword(password)) {
+    return res.status(200).json({ msg: "Password válido" });
+  } else {
+    const error = new Error("Password no válido");
+    return res.status(401).json({ msg: error.message });
+  }
+};
+
+export { registrar, perfil, confirmar, autenticar };
