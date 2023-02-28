5c86d76d65fc205d0b0338fa9ce134293e662bb6
= <=>
2023-02-28 12:51:48 +0100

    CRUD Pacientes

--- backend/routes/pacienteRoutes.js
+++ backend/routes/pacienteRoutes.js
@@ -2,11 +2,23 @@ import express from "express";
 import {
   agregarPaciente,
   obtenerPacientes,
+  obtenerPaciente,
+  actualizarPaciente,
+  borrarPaciente,
 } from "../controllers/pacienteController.js";
 import checkAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(checkAuth, agregarPaciente).get(obtenerPacientes);
+router
+  .route("/")
+  .post(checkAuth, agregarPaciente)
+  .get(checkAuth, obtenerPacientes);
+
+router
+  .route("/:id")
+  .get(checkAuth, obtenerPaciente)
+  .put(checkAuth, actualizarPaciente)
+  .delete(checkAuth, borrarPaciente);
 
 export default router;
