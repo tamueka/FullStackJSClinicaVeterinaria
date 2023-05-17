17bd1d4e25e750abb0906c877ee5153059fb28f4
= <=>
2023-05-17 19:16:34 +0200

    pagina registrar + form + navegacion + estilos

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,7 +1,77 @@
+import { Link } from "react-router-dom";
+
 const Registrar = () => {
   return (
     <>
-      <h1>Registrar</h1>
+      <div>
+        <h1 className="text-indigo-600 font-black text-3xl">
+          Crear tu cuenta y Administra tus
+          <span className="text-black"> pacientes</span>
+        </h1>
+      </div>
+
+      <div className="mt-20 md:mt-5 shadow-lg px-5 rounded-xl bg-white">
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Nombre
+            </label>
+            <input
+              type="text"
+              placeholder="Nombre de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Password
+            </label>
+            <input
+              type="password"
+              placeholder="Tu password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Repetir Password
+            </label>
+            <input
+              type="password"
+              placeholder="Repite Tu password"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Iniciar Sesión"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase
+          mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+          />
+        </form>
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link className="block text-center my-5 text-gray-500" to="/">
+            ¿Ya tienes una cuenta? Inicia Sesión
+          </Link>
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/olvide-password"
+          >
+            Olvidé mi password
+          </Link>
+        </nav>
+      </div>
     </>
   );
 };
