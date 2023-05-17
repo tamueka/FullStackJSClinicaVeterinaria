24373ca4d906c788d9f85632499534637d26af51
= <=>
2023-05-17 19:27:08 +0200

    pagina olvide mi password + form + estilos

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,8 +1,47 @@
+import { Link } from "react-router-dom";
+
 const OlvidePassword = () => {
   return (
     <>
-      <div>1</div>
-      <div>2</div>
+      <div>
+        <h1 className="text-indigo-600 font-black text-3xl">
+          Recupera tu password y Administra tus
+          <span className="text-black"> pacientes</span>
+        </h1>
+      </div>
+
+      <div className="mt-20 md:mt-5 shadow-lg px-5 rounded-xl bg-white">
+        <form>
+          <div className="my-5">
+            <label className="uppercase text-gray-600 block text-xl font-bold">
+              Email
+            </label>
+            <input
+              type="email"
+              placeholder="Email de Registro"
+              className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Recuperar Cuenta"
+            className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase
+          mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+          />
+        </form>
+        <nav className="mt-10 lg:flex lg:justify-between">
+          <Link className="block text-center my-5 text-gray-500" to="/">
+            ¿Ya tienes una cuenta? Inicia Sesión
+          </Link>
+          <Link
+            className="block text-center my-5 text-gray-500"
+            to="/registrar"
+          >
+            ¿No tienes una cuenta registrate?
+          </Link>
+        </nav>
+      </div>
     </>
   );
 };
