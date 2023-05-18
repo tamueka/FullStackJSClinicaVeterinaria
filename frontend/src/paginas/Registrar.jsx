8b20428b3aa700633a75b94d503103e9aea69514
= <=>
2023-05-18 20:00:10 +0200

    validacion formulario registrar + console logs con errores de validacion

--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -6,6 +6,25 @@ const Registrar = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repetirPassword, setRepetirPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if ([nombre, email, password, repetirPassword].includes("")) {
+      console.log("Hay campos vacios");
+      return;
+    }
+    if (password !== repetirPassword) {
+      console.log("Los password no son iguales");
+      return;
+    }
+
+    if (password.length < 6) {
+      console.log("El password es muy corto, agrega minimo 6 caracteres");
+      return;
+    }
+    console.log("Enviar form");
+  };
   return (
     <>
       <div>
@@ -16,7 +35,7 @@ const Registrar = () => {
       </div>
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 rounded-xl bg-white">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">
               Nombre
