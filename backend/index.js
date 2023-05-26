7b0d0c02689658d12da4c7ae78676885f766a641
= <=>
2023-05-26 20:44:51 +0200

    instalamos axios en el frontend para peticiones a backend + instalamos cors en el backend para poder comunicar front-back + peticion a back Registrar Uuario

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import conectarDB from "./config/db.js";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacientesRoutes from "./routes/pacienteRoutes.js";
@@ -14,6 +15,21 @@ dotenv.config();
 
 // Realizamos conexion con Base de Datos
 conectarDB();
+
+const dominiosPermitidos = ["http://localhost:5173"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (dominiosPermitidos.indexOf(origin) !== -1) {
+      // el origen del reques esta permitido
+      callback(null, true);
+    } else {
+      callback(new Error("No permitido por cors"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacientesRoutes);
--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import Alerta from "../components/Alerta";
+import axios from "axios";
 
 const Registrar = () => {
   const [nombre, setNombre] = useState("");
@@ -10,7 +11,7 @@ const Registrar = () => {
 
   const [alerta, setAlerta] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if ([nombre, email, password, repetirPassword].includes("")) {
@@ -32,6 +33,19 @@ const Registrar = () => {
     setAlerta({});
 
     // Crear usuario en la api
+    try {
+      const url = "http://localhost:4000/api/veterinarios";
+      await axios.post(url, { nombre, email, password });
+      setAlerta({
+        msg: "Creado correctamente",
+        error: false,
+      });
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
   };
 
   const { msg } = alerta;
